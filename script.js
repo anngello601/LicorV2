92c024c2df91e4a054836b4a504222ba7fef6b82
lAngeLqI <137748293+lAngeLqI@users.noreply.github.com>
2025-11-13 02:34:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,72 @@ const menuToggle = document.getElementById("menu-toggle");
 
   menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+
   });
+
+// PARTE DE CATEGORIAS // 
+
+const btnVinos = document.getElementById('verMasVinos');
+const catalogoVinos = document.getElementById('catalogo-vinos');
+const cerrarCatalogo = document.getElementById('cerrarCatalogo');
+
+const btnWhisky = document.getElementById('verMasWhisky');
+const catalogoWhisky = document.getElementById('catalogo-whisky');
+const cerrarCatalogoWhisky = document.getElementById('cerrarCatalogoWhisky');
+
+const btnLicores = document.getElementById('verMasLicores');
+const catalogoLicores = document.getElementById('catalogo-Licores');
+const cerrarCatalogoLicores = document.getElementById('cerrarCatalogoLicores');
+
+const btnCerveza= document.getElementById('verMasCerveza');
+const catalogoCerveza = document.getElementById('catalogo-Cerveza');
+const cerrarCatalogoCerveza = document.getElementById('cerrarCatalogoCerveza');
+
+// Mostrar catálogo al presionar "Ver más"
+btnVinos.addEventListener('click', () => {
+  catalogoVinos.style.display = 'flex';
+  document.body.style.overflow = 'hidden'; // Evita scroll del fondo
+});
+
+// Cerrar catálogo
+cerrarCatalogo.addEventListener('click', () => {
+  catalogoVinos.style.display = 'none';
+  document.body.style.overflow = 'auto';
+});
+
+btnWhisky.addEventListener('click', () => {
+  catalogoWhisky.style.display = 'flex';
+  catalogoWhisky.style.animation = 'fadeIn 0.5s ease forwards';
+  document.body.style.overflow = 'hidden';
+});
+
+// Cerrar catálogo
+cerrarCatalogoWhisky.addEventListener('click', () => {
+  catalogoWhisky.style.display = 'none';
+  document.body.style.overflow = 'auto';
+});
+
+btnLicores.addEventListener('click', () => {
+  catalogoLicores.style.display = 'flex';
+  catalogoLicores.style.animation = 'fadeIn 0.5s ease forwards';
+  document.body.style.overflow = 'hidden';
+});
+
+// Cerrar catálogo
+cerrarCatalogoLicores.addEventListener('click', () => {
+  catalogoLicores.style.display = 'none';
+  document.body.style.overflow = 'auto';
+});
+
+
+btnCerveza.addEventListener('click', () => {
+  catalogoCerveza.style.display = 'flex';
+  catalogoCerveza.style.animation = 'fadeIn 0.5s ease forwards';
+  document.body.style.overflow = 'hidden';
+});
+
+// Cerrar catálogo
+cerrarCatalogoCerveza.addEventListener('click', () => {
+  catalogoCerveza.style.display = 'none';
+  document.body.style.overflow = 'auto';
+});
